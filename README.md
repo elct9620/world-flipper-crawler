4e397bc1bcaf2552049ee96f462b4d3a1a293e6b
蒼時弦也 <elct9620@frost.tw>
2019-12-06 11:18:24 +0800

    Setup vue project for front-end

---
+++ README.md
@@ -0,0 +1,3 @@
+# World Flipper Crawler
+
+TODO
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    Comming soon...
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+}
+</style>
